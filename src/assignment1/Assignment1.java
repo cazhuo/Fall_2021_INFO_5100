d33c5a750292059b9d7178e1b4616a8942eb26b9
cazhuo <cazhuo14@gmail.com>
2021-09-29 18:16:21 -0700

    assign1.ver2

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -1,10 +1,13 @@
 package assignment1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Assignment1 {
 
     // Q1:
     // T: O(n); S: O(1)
-    public int sumOfUnique(int[] nums) {
+    public static int sumOfUnique(int[] nums) {
         int[] record = new int[101];
         int sum=0;
         for (int i=0; i<nums.length; i++) {
@@ -19,6 +22,47 @@ public class Assignment1 {
             }
         }
         return sum;
+    }
+
+    // Q2:
+    // T: O(n); S: O(n)
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int beg = 0, end = n-1, i = n-1;
+        while (i>=0) {
+            if (nums[beg] * nums[beg] > nums[end] * nums[end]) {
+                res[i] = nums[beg] * nums[beg];
+                i--;
+                beg ++;
+            } else {
+                res[i] = nums[end] * nums[end];
+                i --;
+                end --;
+            }
+        }
+        return res;
+    }
+
+    // Q3:
+    // T: O(n); S: O(n)
+    public static int firstNonRepeating(int[] arr) {
+        int n = arr.length;
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (m.containsKey(arr[i])) {
+                m.put(arr[i], m.get(arr[i]) + 1);
+            } else {
+                m.put(arr[i], 1);
+            }
+        }
+        // Traverse array again and return first element with count 1.
+        for (int i = 0; i < n; i++) {
+            if (m.get(arr[i]) == 1) {
+                return arr[i];
+            }
+        }
+        return -1;
     }
 
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,21 @@
 package main;
 
+import assignment1.Assignment1;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+        // Q3:
+        int[] arr = new int[] {2, 3, 4, 2, 2, 3, 5, 7};
+        int output = 4;
+
+        int[] inputArray = arr;
+        int q3Output = Assignment1.firstNonRepeating(inputArray);
+        System.out.println("Q3 expected: " + output);
+        System.out.println(q3Output);
+
     }
 }
