a9a0c056cf57949cb8a64377379e762a8a7b11a8
cazhuo <cazhuo14@gmail.com>
2021-10-05 17:17:53 -0700

    assign2.lc3

---
+++ src/assignment2/A2_lc_Q3.java
@@ -0,0 +1,23 @@
+package assignment2;
+
+public class A2_lc_Q3 {
+    // T: O(n); S: 0(1)
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        if (n==1) return;
+
+        int l=0, r=0;
+        int temp;
+        while (r<n) {
+            if (nums[r]==0) {
+                r++;
+            }else{
+                temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+                r++;
+            }
+        }
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,87 +8,92 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
-        // Q1:
-        System.out.println("=============================");
-        int[] q1Input1 = new int[] {1, 2, 3, 2};
-        int q1Expected1 = 4;
-        System.out.println("Q1 expected: " + q1Expected1);
-        System.out.println(Assignment1.sumOfUnique(q1Input1));
-
-        int[] q1Input2 = new int[] {1, 1, 1, 1, 1};
-        int q1Expected2 = 0;
-        System.out.println("Q1 expected: " + q1Expected2);
-        System.out.println(Assignment1.sumOfUnique(q1Input2));
-
-        int[] q1Input3 = new int[]{1, 2, 3, 4, 5};
-        int q1Expected3 = 15;
-        System.out.println("Q1 expected: " + q1Expected3);
-        System.out.println(Assignment1.sumOfUnique(q1Input3));
-
-        // Q2:
-        System.out.println("=============================");
-        int[] q2Input1 = new int[] {-4,-1,0,3,10};
-        int[] q2Expected1 = new int[] {0,1,9,16,100};
-        System.out.println("Q2 expected: " + Arrays.toString(q2Expected1));
-        System.out.println(Arrays.toString(Assignment1.sortedSquares(q2Input1)));
-
-        int[] q2Input2 = new int[] {-7,-3,2,3,11};
-        int[] q2Expected2 = new int[] {4,9,9,49,121};
-        System.out.println("Q2 expected: " + Arrays.toString(q2Expected2));
-        System.out.println(Arrays.toString(Assignment1.sortedSquares(q2Input2)));
-
-        // Q3:
-        System.out.println("=============================");
-        int[] q3Input1 = new int[]{2, 3, 4, 2, 2, 3, 5, 7};
-        int q3Expected1 = 4;
-        System.out.println("Q3 expected: " + q3Expected1);
-        System.out.println(Assignment1.firstNonRepeating(q3Input1));
-
-        int[] q3Input2 = new int[]{77, 9, 4, 2, 4, 77, 9};
-        int q3Expected2 = 2;
-        System.out.println("Q3 expected: " + q3Expected2);
-        System.out.println(Assignment1.firstNonRepeating(q3Input2));
-
-        // Q4:
-        System.out.println("=============================");
-        String q4Input1_1 = "abc";
-        String q4Input1_2 = "cba";
-        boolean q4Expected1 = true;
-        System.out.println("Q4 expected: " + q4Expected1);
-        System.out.println(Assignment1.canConstruct(q4Input1_1, q4Input1_2));
-
-        String q4Input2_1 = "abc";
-        String q4Input2_2 = "cbaaaa";
-        boolean q4Expected2 = true;
-        System.out.println("Q4 expected: " + q4Expected2);
-        System.out.println(Assignment1.canConstruct(q4Input2_1, q4Input2_2));
-
-        String q4Input3_1 = "cbaaaa";
-        String q4Input3_2 = "abc";
-        boolean q4Expected3 = false;
-        System.out.println("Q4 expected: " + q4Expected3);
-        System.out.println(Assignment1.canConstruct(q4Input3_1, q4Input3_2));
-
-        // Q5:
-        System.out.println("=============================");
-        String q5Input1 = "1A3d4s5t";
-        boolean q5Expected1 = true;
-        System.out.println("Q5 expected: " + q5Expected1);
-        System.out.println(Assignment1.isPattern(q5Input1));
-
-        String q5Input2 = "a11b";
-        boolean q5Expected2 = false;
-        System.out.println("Q5 expected: " + q5Expected2);
-        System.out.println(Assignment1.isPattern(q5Input2));
-
-        String q5Input3 = "";
-        boolean q5Expected3 = false;
-        System.out.println("Q5 expected: " + q5Expected3);
-        System.out.println(Assignment1.isPattern(q5Input3));
 
 
+
+
+
+
+//        // ASSIGNMENT 1
+//        // Q1:
+//        System.out.println("=============================");
+//        int[] q1Input1 = new int[] {1, 2, 3, 2};
+//        int q1Expected1 = 4;
+//        System.out.println("Q1 expected: " + q1Expected1);
+//        System.out.println(Assignment1.sumOfUnique(q1Input1));
+//
+//        int[] q1Input2 = new int[] {1, 1, 1, 1, 1};
+//        int q1Expected2 = 0;
+//        System.out.println("Q1 expected: " + q1Expected2);
+//        System.out.println(Assignment1.sumOfUnique(q1Input2));
+//
+//        int[] q1Input3 = new int[]{1, 2, 3, 4, 5};
+//        int q1Expected3 = 15;
+//        System.out.println("Q1 expected: " + q1Expected3);
+//        System.out.println(Assignment1.sumOfUnique(q1Input3));
+//
+//        // Q2:
+//        System.out.println("=============================");
+//        int[] q2Input1 = new int[] {-4,-1,0,3,10};
+//        int[] q2Expected1 = new int[] {0,1,9,16,100};
+//        System.out.println("Q2 expected: " + Arrays.toString(q2Expected1));
+//        System.out.println(Arrays.toString(Assignment1.sortedSquares(q2Input1)));
+//
+//        int[] q2Input2 = new int[] {-7,-3,2,3,11};
+//        int[] q2Expected2 = new int[] {4,9,9,49,121};
+//        System.out.println("Q2 expected: " + Arrays.toString(q2Expected2));
+//        System.out.println(Arrays.toString(Assignment1.sortedSquares(q2Input2)));
+//
+//        // Q3:
+//        System.out.println("=============================");
+//        int[] q3Input1 = new int[]{2, 3, 4, 2, 2, 3, 5, 7};
+//        int q3Expected1 = 4;
+//        System.out.println("Q3 expected: " + q3Expected1);
+//        System.out.println(Assignment1.firstNonRepeating(q3Input1));
+//
+//        int[] q3Input2 = new int[]{77, 9, 4, 2, 4, 77, 9};
+//        int q3Expected2 = 2;
+//        System.out.println("Q3 expected: " + q3Expected2);
+//        System.out.println(Assignment1.firstNonRepeating(q3Input2));
+//
+//        // Q4:
+//        System.out.println("=============================");
+//        String q4Input1_1 = "abc";
+//        String q4Input1_2 = "cba";
+//        boolean q4Expected1 = true;
+//        System.out.println("Q4 expected: " + q4Expected1);
+//        System.out.println(Assignment1.canConstruct(q4Input1_1, q4Input1_2));
+//
+//        String q4Input2_1 = "abc";
+//        String q4Input2_2 = "cbaaaa";
+//        boolean q4Expected2 = true;
+//        System.out.println("Q4 expected: " + q4Expected2);
+//        System.out.println(Assignment1.canConstruct(q4Input2_1, q4Input2_2));
+//
+//        String q4Input3_1 = "cbaaaa";
+//        String q4Input3_2 = "abc";
+//        boolean q4Expected3 = false;
+//        System.out.println("Q4 expected: " + q4Expected3);
+//        System.out.println(Assignment1.canConstruct(q4Input3_1, q4Input3_2));
+//
+//        // Q5:
+//        System.out.println("=============================");
+//        String q5Input1 = "1A3d4s5t";
+//        boolean q5Expected1 = true;
+//        System.out.println("Q5 expected: " + q5Expected1);
+//        System.out.println(Assignment1.isPattern(q5Input1));
+//
+//        String q5Input2 = "a11b";
+//        boolean q5Expected2 = false;
+//        System.out.println("Q5 expected: " + q5Expected2);
+//        System.out.println(Assignment1.isPattern(q5Input2));
+//
+//        String q5Input3 = "";
+//        boolean q5Expected3 = false;
+//        System.out.println("Q5 expected: " + q5Expected3);
+//        System.out.println(Assignment1.isPattern(q5Input3));
+//
+//
 
     }
 
