6c0f4c4fe34e65d0040892d5c13a9a810d5ea38b
cazhuo <cazhuo14@gmail.com>
2021-12-09 21:11:20 -0800

    added Main & Student class

---
+++ src/assignment6/Main.java
@@ -0,0 +1,48 @@
+package assignment6;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Student> students=new ArrayList<>();
+
+        Student s1=new Student(001, "Harry",3.3, parseDate("1998-12-24"));
+        Student s2=new Student(002, "Ron",3.0, parseDate("1997-07-01"));
+        Student s3=new Student(003, "Hermione",4.0, parseDate("1997-10-31"));
+        Student s4=new Student(004, "Draco",3.8, parseDate("1997-11-06"));
+        Student s5=new Student(005, "Luna",3.6, parseDate("1998-01-15"));
+
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        students.add(s5);
+
+        System.out.println("*** Sort by ascending order of 'name' : ***");
+        Collections.sort(students, (a, b) -> a.getName().compareTo(b.getName()));
+        for (Student s : students) System.out.println(s.getName());
+
+        System.out.println("*** Sort by descending order of 'gpa' : ***");
+        Collections.sort(students, (a,b) -> (int)(b.getGpa()*10) - (int)(a.getGpa()*10));
+        for (Student s : students) System.out.println(s.getGpa());
+
+
+        System.out.println("*** Sort by ascending order of 'dateOfBirth' : ***");
+        Collections.sort(students, (a,b) -> a.getDateOfBirth().getYear() - b.getDateOfBirth().getYear());
+        for (Student s : students) System.out.println(s.getDateOfBirth());
+    }
+
+    public static Date parseDate(String date) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+}
---
+++ src/assignment6/Student.java
@@ -0,0 +1,33 @@
+package assignment6;
+
+import java.util.Date;
+
+public class Student {
+    private int id;
+    private String name;
+    private double gpa;
+    private Date dateOfBirth;
+
+    public Student(int id, String name, double gpa, Date dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
